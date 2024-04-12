3a90935766126e1d78bb2b1781b8a422079ac86f
KumarSam <sampank@gmail.com>
2024-04-12 12:23:02 +0100

    Removing compile time errors

--- FeaturesTests.java
+++ FeaturesTests.java
@@ -4,16 +4,6 @@ public class FeaturesTests {
 
     @Test
     public void testExplode() {
-        int result = 0;
-        int resultOfARoll = 0;
-        while(count > 0) {
-            resultOfARoll = DiceRoll.roll(sides);
-            result = result + resultOfARoll;
-            if (resultOfARoll == sides) {
-                count++;
-            } else {
-                count--;
-            }
-        }
+
     }
 }
